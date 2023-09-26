685e72f937f158c5d809d6d6a4a2498805283b81
rihapfirdaus <muhammadrihap448@gmail.com>
2023-09-26 17:46:10 +0700

    update

---
+++ Pertemuan2-HelloToast/app/src/main/java/lat/pam/hellotoast/MainActivity2.kt
@@ -0,0 +1,12 @@
+package lat.pam.hellotoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import lat.pam.hellotoast.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
