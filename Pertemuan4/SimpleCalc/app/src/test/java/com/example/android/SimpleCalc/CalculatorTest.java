d684fde0117084e9a8e0a7dd94f70b5cdc467779
rihapfirdaus <muhammadrihap448@gmail.com>
2023-10-12 05:59:55 +0700

    update

--- Pertemuan4/SimpleCalc/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
+++ Pertemuan4/SimpleCalc/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -16,8 +16,6 @@
 
 package com.example.android.SimpleCalc;
 
-import android.test.suitebuilder.annotation.SmallTest;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,7 +23,9 @@ import org.junit.runners.JUnit4;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static  org.hamcrest.MatcherAssert.assertThat;
+
+import androidx.test.filters.SmallTest;
 
 /**
  * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
@@ -52,7 +52,34 @@ public class CalculatorTest {
         double resultAdd = mCalculator.add(1d, 1d);
         assertThat(resultAdd, is(equalTo(2d)));
     }
-
-
-
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = mCalculator.sub(1d, 1d);
+        assertThat(resultSub, is(equalTo(0d)));
+    }
+    @Test
+    public void subWorksWithNegativeResult() {
+        double resultSub = mCalculator.sub(1d, 17d);
+        assertThat(resultSub, is(equalTo(-16d)));
+    }
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = mCalculator.mul(32d, 2d);
+        assertThat(resultMul, is(equalTo(64d)));
+    }
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = mCalculator.div(32d,2d);
+        assertThat(resultDiv, is(equalTo(16d)));
+    }
+    @Test
+    public void divTwoNumbersZero() {
+        double resultDiv = mCalculator.div(32d,0);
+        assertThat(resultDiv, is(equalTo(Double.POSITIVE_INFINITY)));
+    }
 }
