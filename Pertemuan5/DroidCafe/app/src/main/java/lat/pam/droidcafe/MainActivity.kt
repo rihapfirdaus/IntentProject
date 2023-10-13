dc6cecec10a4bb94d1a312c4d5e7085ef758e4bd
rihapfirdaus <muhammadrihap448@gmail.com>
2023-10-13 13:00:50 +0700

    upload new submission

---
+++ Pertemuan5/DroidCafe/app/src/main/java/lat/pam/droidcafe/MainActivity.kt
@@ -0,0 +1,37 @@
+package lat.pam.droidcafe
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import lat.pam.droidcafe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            val intent = Intent(this, OrderActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
